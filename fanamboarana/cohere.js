2634edd17baa1efdaf5e273f86f185842576b0f2
Hajaena <jainajex@gmail.com>
2025-11-12 23:13:58 +0300

    command-r

--- fanamboarana/cohere.js
+++ fanamboarana/cohere.js
@@ -5,7 +5,7 @@ const cohere = new CohereClient({ apiKey: process.env.COHERE_API_KEY });
 async function generateWithCohere(tany_fanoratana) {
   try {
     const response = await cohere.chat({
-      model: 'command-r-plus',
+      model: 'command-r',
       message: tany_fanoratana,
       temperature: 0.7,
       max_tokens: 250,
