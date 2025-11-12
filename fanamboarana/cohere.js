4ee43e2658e2b90509156072a6f335925bda546e
Hajaena <jainajex@gmail.com>
2025-11-12 22:59:42 +0300

    Mise à jour vers cohere sept 2025

--- fanamboarana/cohere.js
+++ fanamboarana/cohere.js
@@ -3,7 +3,7 @@ const { CohereClient } = require('cohere-ai');
 const cohere = new CohereClient({ apiKey: process.env.COHERE_API_KEY });
 
 async function generateWithCohere(tany_fanoratana) {
-  const response = await cohere.generate({
+  const response = await cohere.chat({
     model: 'command-r-plus',
     prompt: tany_fanoratana,
     max_tokens: 250,
